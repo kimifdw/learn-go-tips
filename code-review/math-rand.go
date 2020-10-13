9277cc1a7f9852562b9c3c8c212b6d24636e89a9
fudongwei <fdw911cool@qq.com>
2020-10-13 23:28:30 +0800

    增加code-review事例

---
+++ code-review/math-rand.go
@@ -0,0 +1,16 @@
+package code_review
+
+import (
+	"crypto/rand"
+	"fmt"
+)
+
+func Key() string {
+	buf := make([]byte, 16)
+	_, err := rand.Read(buf)
+	if err != nil {
+		panic(err)
+	}
+
+	return fmt.Sprintf("%x", buf)
+}
